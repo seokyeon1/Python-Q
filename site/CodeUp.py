fb5994a0af475ea6d55727fbb62993250fcb9431
seokyeoon <jangseokyeon1110@gmail.com>
2021-08-05 16:44:49 +0900

    Add my Python

---
+++ site/CodeUp.py
@@ -0,0 +1,575 @@
+# 코드업 6001 print("Hello")
+
+# 코드업 6002 print("Hello World")
+
+# 코드업 6003 print("Hello")
+#             print("World")
+
+# 코드업 6004 print("'""Hello""'")
+
+# 코드업 6005 print('"Hello World"')
+
+# 코드업 6006 print("\"!@#$%^&*()\'")
+
+# 코드업 6007 print('"C:\\Download\\\'hello\'.py"')
+
+# 코드업 6008 print("print("'"Hello\\nWorld"'")")
+
+# 코드업 6009 English = input()
+#             print(English)
+
+# 코드업 6010 number = input()
+#             print(number)
+
+# 코드업 6011 number = float(input())
+#                      print(number)
+
+# 코드업 6012 number = input()
+#             number2 = input()
+#             print(number)
+#             print(number2)
+
+# 코드업 6013 a = input()
+#             b = input()
+#             print(b+"\n"+a)
+
+# 코드업 6014 number = input()
+#             print(float(number))
+#             print(float(number))
+#             print(float(number))
+
+# 코드업 6015 number,number2 = map(int, input().split())
+#             print(number, number2)
+
+# 코드업 6016 a,b = input().split()
+#             print(b, a)
+
+# 코드업 6017 a = input()
+#             print(a, a, a)
+
+# 코드업 6018 a, b = input().split(':')
+#             print(a, b, sep = ':')
+
+# 코드업 6019 y,m,d = input().split('.')
+#             print(d,m,y, sep='-')
+
+# 코드업 6020 a,b = input().split('-')
+#             print(a, b, sep = '')
+
+# 코드업 6021 s = input()
+#             print(s[0])
+#             print(s[1])
+#             print(s[2])
+#             print(s[3])
+#             print(s[4])
+
+# 코드업 6022 s = input()
+#             print(s[0:2], s[2:4], s[4:6])
+
+# 코드업 6023 h,m,s = input().split(':')
+#             print(m)
+
+# 코드업 6024 w1, w2 = input().split(' ')
+#             n = w1 + w2
+#             print(n)
+
+# 코드업 6025 num, num2 = input().split()
+#             result = int(num) + int(num2)
+#             print(result)
+
+# 코드업 6026 a = input()
+#             b = input()
+#             a = float(a)
+#             b = float(b)
+#             result = float(a) + float(b)
+#             print(result)
+
+# 코드업 6027 a = input()
+#             n = int(a)
+#             print("%x"%n)
+
+# 코드업 6028 a = input()
+#             n = int(a)
+#             print("%X"%n)
+
+# 코드업 6029 a = input()
+#             n = int(a, 16)
+#             print("%o"%n)
+
+# 코드업 6030 n = ord(input())
+#             print(n)
+
+# 코드업 6031 c = int(input())
+#            print(chr(c))
+
+
+# 코드업 6032 c = int(input())
+#             print(-c)
+
+# 코드업 6033 c = ord(input())
+#             print(chr(c + 1))
+
+# 코드업 6034 a,b = input().split()
+#             c = int(a) - int(b)
+#             print(c)
+
+# 코드업 6035 a,b = input().split()
+#             a = float(a)
+#             b = float(b)
+#             multiply = a * b
+#             print(multiply)
+
+# 코드업 6036 w,n = input().split()
+#             print(w*int(n))
+
+# 코드업 6037 n = input()
+#             s = input()
+#             print(int(n) * s)
+
+# 코드업 6038 a, b = input().split()
+#             c = int(a)**int(b)
+#             print(c) 
+
+# 코드업 6039 a = float()
+#             b = float()
+#             a, b = input().split()
+#             c = float(a)**float(b)
+#             print(c) 
+
+# 코드업 6040 a,b = input().split()
+#             c = (int(a)//int(b))
+#             print(c)
+
+# 코드업 6041 a,b = input().split()
+#             c = (int(a)%int(b))
+#             print(c)
+
+# 코드업 6042 a = float(input())
+#   
+#           print (round(a, 2))
+
+# 코드업 6043 a,b = input().split()
+#             a = float(a)
+#             b = float(b)
+#             c = a/b
+#             print ('%.3f' %c)
+
+# 코드업 6044 a,b = input().split()
+#             a = int(a)
+#             b = int(b)
+#             c = a/b
+#             print(a+b)
+#             print(a-b)
+#             print(a*b)
+#             print(a//b)
+#             print(a%b)
+#             print(round(c,2))
+
+# 코드업 6045 a,b,c = input().split()
+#             a = int(a)
+#             b = int(b)
+#             c = int(c)
+#             d = a + b + c
+#             e = (a + b + c) / 3
+#             print(d, format(e, ".2f"))
+
+# 코드업 6046 a = input()
+#             a = int(a)
+#             print(a*2)
+
+# 코드업 6047 a,b = map(int,input().split())
+#             c = 2**b
+#             print(a*c)
+
+# 코드업 6048 a,b = input().split()
+#             a = int(a)
+#             b = int(b)
+#             print(a < b)
+
+# 코드업 6049 a,b = input().split()
+#             a = int(a)
+#             b = int(b)
+#             print(a == b)
+
+# 코드업 6050 a,b = input().split()
+#             a = int(a)
+#             b = int(b)
+#             print(b >= a)
+
+# 코드업 6051 a,b = input().split()
+#             a = int(a)
+#             b = int(b)
+#             print(a != b)
+
+# 코드업 6052 a = int(input())
+#             print(bool(a))
+
+# 코드업 6053 a = bool(int(input()))
+#             if (a == True):
+#               print("False")
+#             else:
+#               print("True")
+
+# 코드업 6054 a,b = map(int, input().split())
+
+#             if(bool(a) and bool(b) == True):
+#                 print("True")
+#             else:
+#                 print("False")    
+
+# 코드업 6055 a,b = map(int, input().split())
+
+#             if(bool(a) or bool(b) == True):
+#                 print("True")
+#             else:
+#                 print("False")    
+
+# 코드업 6056 a,b = map(int, input().split())
+#             if(int(a) != int(b)):
+#                 print("True")
+#             else:
+#                 print("False")
+
+# 코드업 6057 a,b = map(int, input().split())
+#             if(int(a) == int(b)):
+#                 print("True")
+#             else:
+#                 print("False")
+
+# 코드업 6058 a,b = map(int, input().split())
+#             a,b = bool(a), bool(b)
+#             if(a == False and b == False):
+#                 print("True")
+#             else:
+#                 print("False")
+
+# 코드업 6059 a = input()
+#             a = int(a)
+#             print(~a)
+
+# 코드업 6060 a, b = map(int, input().split())
+#             print(a&b)
+
+# 코드업 6061 a, b = map(int, input().split())
+#             print(a|b)
+
+# 코드업 6062 a, b = map(int, input().split())
+#             print(a^b)
+
+# 코드업 6063 a,b = map(int, input().split())
+#             if(a>=b):
+#               print(a)
+#             else:
+#               print(b)
+
+# 코드업 6064 a,b,c = map(int, input().split())
+#             if(a >= b):
+#                if(b >= c):
+#                   print(c)
+#             else:
+#                  print(b)
+#        else:
+#           if(a >= c):
+#               print(c)
+#           else:
+#               print(a)                
+
+# 코드업 6065 a,b,c = map(int, input().split())
+#             if(a % 2 == 0):
+#               print(a)
+#             if(b % 2 == 0):
+#               print(b)
+#             if(c % 2 == 0):
+#               print(c)    
+
+# 코드업 6066 a,b,c = map(int, input().split())
+#             if(a % 2 == 0):
+#               print("even")
+#             else:
+#               print("odd")
+#             if(b % 2 == 0):
+#               print("even")
+#             else:
+#               print("odd")
+#             if(c % 2 == 0):
+#               print("even")
+#             else:
+#               print("odd")
+
+# 코드업 6067 a = int(input())
+#             if(a < 0):
+#               if(a % 2 == 0):
+#                   print("A")
+#               else:
+#                   print("B")
+#             else:
+#               if(a % 2 == 0):
+#                   print("C")
+#               else:
+#                   print("D")    
+
+# 코드업 6068 a = int(input())
+#             if(a >= 90):
+#               print("A")
+#             elif(a >= 70):
+#               print("B")
+#             elif(a >= 40):
+#               print("C")
+#             elif(a >= 0):
+#               print("D")
+
+# 코드업 6069 a = input()
+#             if(a == 'A'):
+#               print("best!!!")
+#             elif(a == 'B'):
+#               print("good!!")
+#             elif(a == 'C'):
+#               print("run!")
+#             elif(a == 'D'):
+#               print("slowly~")
+#             else:
+#               print("what?")    
+
+# 여기부터 틀린거 체크
+
+# 코드업 6070 a = int(input())
+# if a//3==1:
+#     print("spring")
+# elif a//3==2:
+#     print("summer")
+# elif a//3==3:
+#     print("fall")
+# else:
+#     print("winter")
+
+# 코드업 6071 n=int(input())
+# while n!=0:
+#     print(n)
+#     n=int(input())
+
+# 코드업 6072 n=int(input())
+# while n!=0:
+#     print(n)
+#     n=n-1
+
+# 코드업 6073 n=int(input())
+# while n!=0:
+#     n-=1
+#     print(n)
+
+# 코드업 6074 c = ord(input())
+# t = ord('a')
+# while t<=c :
+#   print(chr(t), end=' ')
+#   t += 1
+
+# 코드업 6075, 6076 n = int(input())
+# a=n
+# while n>=0:
+#     print(a-n)
+#     n-=1  
+
+# 코드업 6077 n = int(input())
+# s = 0
+# for i in range(1, n + 1):
+#     if i % 2 == 0 :
+#         s+=i
+# print(s)
+
+# 코드업 6078 while True:
+#     x = input()
+#     print(x)
+#     if(x == 'q'):
+#         break
+
+# 코드업 6079 i = 0
+# sum = 0
+# n = int(input())
+# while True:
+#     i += 1
+#     sum+=i
+#     if(sum>=n):
+#         print(i)
+#         break
+
+# 코드업 6080 a,b = map(int,input().split())
+# i=1
+# j=1
+# for i in range(1, a+1):
+#     for j in range(1, b+1):
+#         print(i,j)
+
+# 코드업 6081 a = int(input(),16)
+# for i in range(1,16):
+#     print('%X*%X=%X'%(a,i,a*i))
+
+# 코드업 6082 a = int(input())
+# for i in range(1,a+1):
+#     if (i%10==3 or i%10==6 or i%10==9):
+#         print('X',end=' ')
+#         continue
+#     print(i,end=' ')
+
+# 코드업 6083 a,b,c = map(int, input().split())
+# count = 0
+# for i in range(a):
+#     for j in range(b):
+#         for k in range(c):
+#             print('%d %d %d'%(i,j,k))
+#             count+=1
+# print(count)
+
+# 코드업 6084 h,b,c,s = map(int,input().split())
+# m = (h* b * c * s)/8/1024/1024
+# print('%.1f MB' %m)
+
+# 코드업 6085 a,b,c = map(int, input().split())
+# res = (a * b * c)/8/1024/1024
+# print('%.2f'%res,"MB")
+
+# 코드업 6086 a=int(input()) 
+# s=0 
+# c=0 
+# while True: 
+#     s=s+c 
+#     c=c+1 
+#     if s>=a: 
+#         break 
+# print(s)
+
+# 코드업 6087 n=int(input())
+# for i in range(1,n+1):
+#     if i%3==0:
+#         continue
+#     print(i,end=' ')
+
+# 코드업 6088 a,d,n = map(int,input().split())
+# total=a
+# for i in range(0,n-1):
+#     total=total+d
+# print(total)
+
+# 코드업 6089 a,r,n = map(int,input().split())
+# total = a
+# for i in range(0,n-1):
+#     total=total*r
+# print(total)
+
+# 코드업 6090 a,m,d,n = map(int,input().split())
+# total = a
+# for i in range(0,n-1):
+#     total=total*m+d
+# print(total)
+
+# 백준 1373 print(oct(int(input(), 2))[2:])
+
+# 백준 10808 x = list(str(input()))
+# alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# for i in range(len(alphabet)):
+#     y = x.count(alphabet[i])
+#     print(y, end=' ')
+
+# 백준 10820 import sys
+# while True: 
+#     line = sys.stdin.readline().rstrip('\n') 
+#     up, lo, sp, nu = 0, 0, 0, 0 
+#     if not line: 
+#         break
+#     for l in line: 
+#             if l.isupper(): 
+#                 up += 1 
+#             elif l.islower():
+#                 lo += 1 
+#             elif l.isdigit():
+#                 nu += 1 
+#             elif l.isspace():
+#                 sp += 1
+#     sys.stdout.write("{} {} {} {}\n".format(lo, up, nu, sp))
+
+# 코드업 6091 a,c,b = map(int, input().split())
+# d = 1
+# while d%a!=0 or d%b!=0 or d%c!=0 :
+#   d += 1
+# print(d)
+
+# 코드업 6092 n= int(input())
+# a=list(map(int,input().split()))
+# d=[]
+# for i in range(24):
+#     d.append(0)
+# for i in range(n):
+#     d[a[i]]+=1
+# for i in range(1,24):
+#     print(d[i],end=' ')
+
+# 코드업 6093 n = int(input())
+# a = list(map(int, input().split()))
+
+# for i in range(n-1,-1,-1):
+
+#     print(a[i],end=' ')
+
+# 코드업 6094 n = int(input())
+# k = list(map(int,input().split()))
+# a=k[0]
+# for i in range(n):
+#     if(a>k[i]):
+#         a=k[i]
+# print(a)
+
+# 코드업 6095 location = []
+# for i in range(20):
+#   location.append([])
+
+#   for j in range(20): 
+#     location[i].append(0) 
+
+# n = int(input()) 
+
+# for i in range(n): 
+#    x, y = map(int, input().split()) 
+#    location[x][y] = 1
+
+# for i in range(1, 20): 
+#   for j in range(1, 20): 
+#     print(location[i][j], end=" ")     
+    
+#   print()
+
+# 백준 11721 a = input()
+# d = len(a)
+# for b in range(0,d,10):
+#     e=b+10
+#     print(a[b:e])
+
+# 백준 11718, 11719 while True:
+#     try:
+#         print(input())
+#     except EOFError:
+#         break
+
+# 코드업 6096 a = [[]*19 for _ in range(19)]
+# for i in range(19):
+#    a[i]=list(map(int,input().split()))
+
+# n = int(input())
+
+# for i in range(n):
+#     b,c=map(int,input().split())
+    
+#     for j in range(19):
+#         if(a[b-1][j]==1):
+#             a[b-1][j]=0
+#         else: a[b-1][j]=1
+    
+#     for j in range(19):
+#         if(a[j][c-1]==1):
+#             a[j][c-1]=0
+#         else: a[j][c-1]=1
+
+# for i in range(19):
+#     for j in range(19):
+#         print(a[i][j],end=' ')
+#     print()
+
+print("Hello, Python 3")
